f2ab0c4551716f7ddebd61c6e1aac0cc2d8bd582
Swapnil <swapnildargude0007@gmail.com>
2019-06-23 15:40:54 +0530

    Xamarin Hybrid App

---
+++ XamarinHybrid/XamarinHybrid.Android/HybridWebViewRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Content;
+using XamarinHybrid;
+using XamarinHybrid.Droid;
+using ANDROID = Android;
+[assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
+namespace XamarinHybrid.Droid
+{
+    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, ANDROID.Webkit.WebView>
+    {
+        const string JavascriptFunction = "function invokeCSharpAction(data){jsBridge.invokeAction(data);}";
+        Context _context;
+
+        public HybridWebViewRenderer(Context context) : base(context)
+        {
+            _context = context;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<HybridWebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var webView = new ANDROID.Webkit.WebView(_context);
+                webView.Settings.JavaScriptEnabled = true;
+                webView.SetWebViewClient(new JavascriptWebViewClient($"javascript: {JavascriptFunction}"));
+                SetNativeControl(webView);
+            }
+            if (e.OldElement != null)
+            {
+                Control.RemoveJavascriptInterface("jsBridge");
+                var hybridWebView = e.OldElement as HybridWebView;
+                hybridWebView.Cleanup();
+            }
+            if (e.NewElement != null)
+            {
+                Control.AddJavascriptInterface(new JSBridge(this), "jsBridge");
+                Control.LoadUrl(Element.Uri);
+                //Control.LoadUrl($"file:///android_asset/Content/{Element.Uri}");
+            }
+        }
+    }
+}
